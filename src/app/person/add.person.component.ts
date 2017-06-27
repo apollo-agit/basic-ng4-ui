b8c19086cec873d594178ab19362130f07a32daf
Ian Hamilton <ian.hamilton.it@gmail.com>
2017-06-27 10:55:36 -0700

    added a listener to list added persons

--- src/app/person/add.person.component.ts
+++ src/app/person/add.person.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FluxReducer } from '../common/flux.reducer';
 import { PersonDataModel, create} from './person.data.model';
 
@@ -7,9 +7,11 @@ import { PersonDataModel, create} from './person.data.model';
   templateUrl: './add.person.component.html'
 })
 
-export class AddPersonComponent {
+export class AddPersonComponent implements OnInit {
 
 	formObject: PersonDataModel;
+
+	allAddUsersObject: Array<String>;
 
 	genderOptions = ['Male', 'Female'];
 
@@ -19,9 +21,30 @@ export class AddPersonComponent {
 		this.formObject = create();
 	}
 
+	ngOnInit() {
+			this._localStoragereducer.backingObject.subscribe(data => {
+			if (data) {
+				this.displayAllAddedUsers(data);
+			}
+		});
+
+		this._localStoragereducer.load();
+	}
+
 	onAddClick() {
 		this._localStoragereducer.add(this.formObject);
 		this.formObject = create();
+	}
+
+	displayAllAddedUsers(personData: Array<PersonDataModel>) {
+		this.allAddUsersObject = new Array<String>();
+
+		for (var i = 0; i < personData.length; i++)
+		{
+		  let person: PersonDataModel = personData[i];
+		  let name = person.firstName + ' ' + person.lastName;
+		  this.allAddUsersObject.push(name);
+		}
 	}
 
 }
